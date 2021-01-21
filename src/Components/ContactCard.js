9aa3fd3b506c7a2b1ae5ee138b485f69eebfdfd2
Jackie Lin <jackie@Jackies-MacBook-Pro.local>
2021-01-20 20:14:08 -0500

    updated

--- src/Components/ContactCard.js
+++ src/Components/ContactCard.js
@@ -9,12 +9,14 @@ class ContactCard extends Component{
         }
     }
     componentDidMount(){
-        let link="#";
-        if(this.props.contactInfo.linkedin.length>0 && this.props.contactInfo.linkedin.indexOf(".com") > -1) link = this.props.contactInfo.linkedin;
-        else if(this.props.contactInfo.twitter.length>0 && this.props.contactInfo.twitter.indexOf(".com") > -1) link = this.props.contactInfo.twitter;
-        else if(this.props.contactInfo.github.length>0 && this.props.contactInfo.github.indexOf(".com") > -1) link = this.props.contactInfo.github;
-        if(link.length>4 && link.indexOf("http://") == -1) link = "http://"+link;
-        this.setState({link})
+        // let link="#";
+        // if(this.props.contactInfo.linkedin.length>0 && this.props.contactInfo.linkedin.indexOf(".com") > -1) link = this.props.contactInfo.linkedin;
+        // else if(this.props.contactInfo.twitter.length>0 && this.props.contactInfo.twitter.indexOf(".com") > -1) link = this.props.contactInfo.twitter;
+        // else if(this.props.contactInfo.github.length>0 && this.props.contactInfo.github.indexOf(".com") > -1) link = this.props.contactInfo.github;
+        // if(link.length>4 && link.indexOf("http://") == -1) link = "http://"+link;
+        // this.setState({link})
+        console.log(this.props.contactInfo.contactEmail && this.props.contactInfo.contactEmail.length>0 ? "mailto: "+this.props.contactInfo.contactEmail: "#")
+        this.setState({link:(this.props.contactInfo.contactEmail && this.props.contactInfo.contactEmail.length>0 ? "mailto: "+this.props.contactInfo.contactEmail: "#")})
     }
     render(){
         return (
@@ -28,7 +30,7 @@ class ContactCard extends Component{
                 <center>
                     <div className="contactCardInfoContainer">
                         <h3>{this.props.contactInfo.firstName} {this.props.contactInfo.lastName}</h3><br/>
-                        <a target="_blank" href={this.state.link}>Contact Link</a>
+                        <a href={this.state.link}>Contact Link</a>
                     </div>
                 </center>
             </div>
